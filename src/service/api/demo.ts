c5990af90739d472ab274d8585a95199fa762e52
燕博文 <349952469@qq.com>
2022-06-09 10:12:56 +0800

    chore(deps): 更新依赖

--- src/service/api/demo.ts
+++ src/service/api/demo.ts
@@ -2,8 +2,8 @@ import { ContentTypeEnum } from '@/enum';
 import { post } from '../request';
 
 /** 示例 */
-export async function fetchDemo(data: any) {
-  return await post<{ hello: string }>('/route/to/demo', data, {
+export async function fetchDemo<T>(data: any) {
+  return await post<T>('/route/to/demo', data, {
     useErrMsg: true,
     contentType: ContentTypeEnum.FORM_URLENCODED
   });
